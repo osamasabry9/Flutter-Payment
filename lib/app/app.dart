6c010c480b3bf2bbdac5dc9efb349a48d7e5912f
osamasabry9 <osamasabry324@gmail.com>
2023-03-03 19:21:42 +0200

    app file

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../core/utils/routes_manager.dart';
+import '../core/utils/theme_manager.dart';
+
 class MyApp extends StatelessWidget {
-  const MyApp._internal(); // name constructor
-  static const MyApp _instance =
-      MyApp._internal(); // Singleton or single instance
-  factory MyApp() => _instance; // factory
+  const MyApp._internal();
+  static const MyApp _instance = MyApp._internal();
+  factory MyApp() => _instance;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Payment App',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const Scaffold(
-          body: Text('Home Page'),
-        ));
+      title: 'Payment App',
+      debugShowCheckedModeBanner: false,
+      theme: getApplicationTheme(),
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.registerRoute,
+    );
   }
 }
